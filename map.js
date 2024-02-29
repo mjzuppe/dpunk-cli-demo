960acc5a134c2471e3dff08fa683e95fb57eff5a
Mark Zuppe <zuppe@live.com>
2024-02-29 12:39:54 -0500

    feat: :fire: Add pre-deployment demo view

--- map.js
+++ map.js
@@ -26,7 +26,22 @@ const list = async () => {
    ])))
 };
 
-const view = () => {};
+const view = () => {
+
+        console.log(chalk.bold('Global Components'));
+        console.log((chalkTable([
+         {ID: "WVYOJO9IITVX", Componenent: "IAM Role", Description: "New role for admin access to workload", Status: chalk.yellowBright('Staged')},
+         {ID: "R9JE3RS6QT3A", Componenent: "CloudFront Distribution", Description: "CDN to accelerate site performance", Status: chalk.yellowBright('Staged')},
+         {ID: "PYPGYEDXKWMP", Componenent: "WAF", Description: "Firewall to prevent common attacks", Status: chalk.yellowBright('Staged') }
+        ])));
+        console.log(chalk.bold('Regional VPC Components'));
+        console.log((chalkTable([
+         {ID: "SPHVOISZNNE4", Componenent: "VPC", Description: "Virtual private cloud", Region: "us-west-2", Status: chalk.yellowBright('Staged')},
+         {ID: "9SD3DM2E88XV", Componenent: "Subnet", Description: "Logical private network", Region: "us-west-2", Status: chalk.yellowBright('Staged')},
+         {ID: "US8N485DFQ56", Componenent: "Lambda Function",Description: "Serverless hosting for application", Region: "us-west-2", address: "", Status: chalk.yellowBright('Staged')},
+         {ID: "CJF0BN1FB3PW", Componenent: "S3 Bucket", Description: "Object storage for logs",Region: "us-west-2", address: "", Status: chalk.yellowBright('Staged') }
+        ])));
+};
 
 const listSolo = chalkTable([{Name: "dpunk-launch", Status: chalk.yellowBright('Staged'), Region: "us-west-2", URL: ""}]);
 
@@ -68,6 +83,7 @@ const map = {
         {name: 'delete', description: 'Delete a dpunk project', action: ()=> actions.delete()},
         {name: 'list', description: 'List all dpunk projects', action: ()=> actions.list()},
         {name: 'deploy', description: 'Deploy a dpunk project', action: ()=> actions.deploy()},
+        {name: 'view', description: 'View a dpunk project', action: ()=> actions.view()}
     ],
     options: [
 
